91b69a7d967ec61bd4bd3469a4b8d8b427aa29a0
Ali <ahsana1045@gmail.com>
2025-02-10 14:45:32 -0800

    Adding Liked_Songs as special_type to Playlist model

--- app/models/like.py
+++ app/models/like.py
@@ -1,6 +1,8 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func, DateTime
+from models.playlist import Playlist
+
 
 
 
@@ -19,5 +21,25 @@ class Like(db.Model):
     #RELATIONSHIPS
     user = db.relationship("User", back_populates="likes")
     song = db.relationship("Song", back_populates="likes")
-    libraries = db.relationship("Library", back_populates="like")
-    
+    # libraries = db.relationship("Library", back_populates="like")
+
+    # When fetching the Liked Songs playlist: Query all likes for the user and return the corresponding songs.
+    # "liked_songs = Like.query.filter_by(user_id=current_user.id).join(Song).all()"
+    def __init__(self, user_id, song_id):
+        self.user_id = user_id
+        self.song_id = song_id
+
+        # Add to Liked Songs playlist
+        liked_playlist = Playlist.query.filter_by(user_id=user_id, special_type='liked_songs').first()
+        if not liked_playlist:
+            liked_playlist = Playlist(
+                user_id=user_id,
+                title="Liked Songs",
+                image_url="default_liked_image_url",  # Add a default image
+                special_type="liked_songs"
+            )
+            db.session.add(liked_playlist)
+            db.session.commit()
+
+        playlist_song = PlaylistSong(playlist_id=liked_playlist.id, song_id=song_id)
+        db.session.add(playlist_song)
