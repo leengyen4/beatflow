664bcea7445038ad770e7dabdeb44c4e61a61752
leengyen4 <leengyen5@gmail.com>
2025-02-10 15:40:25 -0800

    seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,37 +5,32 @@ from .seed_albums import seed_albums, undo_albums
 from .seed_playlists import seed_playlists, undo_playlists
 from .seed_likes import seed_likes, undo_likes
 from .seed_notifications import seed_notifications, undo_notifications
+from .seed_genre import seed_genres, undo_genres
 
 from app.models.db import db, environment, SCHEMA
 
-# Creates a seed group to hold our commands
-# So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
 
-# Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will truncate all tables prefixed with 
-        # the schema name (see comment in users.py undo_users function).
-        # Make sure to add all your other model's undo functions below
         undo_users()
         undo_songs()
         undo_albums()
         undo_playlists()
         undo_likes()
         undo_notifications()
+        undo_genres()  
     seed_users()
     seed_songs()
     seed_albums()
     seed_playlists()
     seed_likes()
     seed_notifications()
+    seed_genres()
 
 
-# Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
@@ -44,3 +39,4 @@ def undo():
     undo_playlists()
     undo_likes()
     undo_notifications()
+    undo_genres() 
