82dceb18ab3df8a32acf1a11ebf552c5f5b4cc9e
Ethan Wong <wngethn256@gmail.com>
2025-02-12 16:09:33 -0500

    adjusted url_prefix to match songs routes

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -3,16 +3,16 @@ from flask_login import login_required, current_user
 from app.models import db
 from app.models import Like, Song
 
-like_routes = Blueprint("likes", __name__)
+like_routes = Blueprint("likes", __name__, url_prefix="/api/songs")
 
 # GET User can see the number of likes on a song
-@like_routes.route('/songs/<int:song_id>/likes', methods=['GET'])
+@like_routes.route('/<int:song_id>/likes', methods=['GET'])
 def get_likes(song_id):
     likes = Like.query.filter_by(song_id=song_id).count()
     return jsonify({"song_id": song_id, "likes": likes})
 
 # POST User can like a song
-@like_routes.route('/songs/<int:song_id>/likes', methods=['POST'])
+@like_routes.route('/<int:song_id>/likes', methods=['POST'])
 @login_required
 def like_song(song_id):
     like_exists = Like.query.filter_by(user_id= current_user.id, song_id=song_id).first()
@@ -27,7 +27,7 @@ def like_song(song_id):
     return jsonify({"message": "User has successfully liked this song!", "song_id":song_id}), 201
 
 # DELETE User can unlike a song
-@like_routes.route('/songs/<int:song_id>/likes', methods=['DELETE'])
+@like_routes.route('/<int:song_id>/likes', methods=['DELETE'])
 @login_required
 def unlike_song(song_id):
     like = Like.query.filter_by(user_id=current_user.id, song_id=song_id).first()
